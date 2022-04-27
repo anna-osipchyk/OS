330fb6627dae6fe60f8e765c6024daf483c7bd91
Anna Osipchik <82057714+anna-osipchyk@users.noreply.github.com>
2022-04-28 00:16:03 +0300

    Create Reporter.cpp

---
+++ lab1/Reporter.cpp
@@ -0,0 +1,53 @@
+include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <list>
+
+
+#pragma warning(disable : 4996)
+
+using namespace std;
+
+struct Employee
+{
+	int num;
+	char name[10];
+	double hours;
+};
+
+list<Employee> readEmployees(char* fileName) {
+	ifstream in;
+	in.open(fileName, ios::binary);
+	list<Employee> employees;
+
+	while (in.peek() != EOF)
+	{
+		Employee employee;
+		in.read((char*)&employee, sizeof(Employee));
+		employees.push_back(employee);
+	}
+
+	in.close();
+
+	return employees;
+}
+
+bool employeeComparator(const Employee& employee1, const Employee& employee2) {
+	return employee1.num < employee2.num;
+}
+
+int main(int argc, char* argv[]) {
+	ofstream out;
+	out.open(argv[2]);
+	double pay = atof(argv[3]);
+
+	list<Employee> employees = readEmployees(argv[1]);
+	employees.sort(employeeComparator);
+
+	out << "Data: " " << argv[0] << "\"\n";
+	for (auto const& iterator : employees) {
+		out << iterator.num << "\t" << iterator.name << "\t" << iterator.hours * pay << "\n";
+	}
+	out.close();
+	return 0;
+}
