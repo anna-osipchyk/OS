c703938719b3ec99c1be9a47c54714ae7cb860dc
Anna Osipchik <82057714+anna-osipchyk@users.noreply.github.com>
2022-05-29 23:27:25 +0300

    Update Source.cpp

--- lab2/Source.cpp
+++ lab2/Source.cpp
@@ -2,45 +2,37 @@
 #include <windows.h>
 #include <vector>
 #include <iterator>
-
-using namespace std;
-
-int _size;
-vector<int> arr;
-
-volatile int _min;
-volatile int _max;
-volatile double avg;
+#include "array.h"
 
 
-DWORD WINAPI min_max(void * _arr) {
-	int* arr = (int *)_arr;
-	_min = arr[0];
-	_max = arr[0];
-	for (int i = 0; i < _size; i++) {
-		if (arr[i] > _max) {
-			_max = arr[i];
+DWORD WINAPI min_max(void * _arr, int size) {
+	myArray* array = static_cast<myArray*>(_arr);
+	int min = arr[0];
+	int max = arr[0];
+	for (int i = 0; i < size; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
 		}
 		Sleep(7);
 		if (arr[i] < _min) {
-			_min = arr[i];
+			min = arr[i];
 		}
 		Sleep(7);
 	}
-	cout << "Min: " << _min << "\n";
-	cout << "Max: " << _max << "\n";
+	std::cout << "Min: " << min << "\n";
+	std::cout << "Max: " << max << "\n";
 	return 0;
 }
 
 DWORD WINAPI average(void * _arr) {
-	int* arr = (int*)_arr;
+myArray* array = static_cast<myArray*>(_arr);
 	int sum = 0;
 	for (int i = 0; i < _size; i++) {
 		sum += arr[i];
 		Sleep(12);
 	}
 	avg = 1. * sum / _size;
-	cout << "Average: " << avg << "\n";
+	std::cout << "Average: " << avg << "\n";
 	return 0;
 }
 
@@ -53,20 +45,22 @@ int main() {
 	DWORD minMaxIDThread;
 	HANDLE averageHThread;
 	DWORD averageIDThread;
-	int* arr;
+
+	int size;
 
 
-
-
-	cout << "Enter size of array:\n";
-	cin >> _size;
-	arr = new int[_size];
+	std::cout << "Enter size of array:\n";
+	cin >> size;
+	myArray *arr = new int[size];
+	array->storage = new int[size];
+    	array->size = size;
 	cout << "Enter elements of array:\n";
 
-	for (int i = 0; i < _size; i++) {
-		cin >> arr[i];
-	}
-
+	for(int i = 0; i < size; i = -~i) {
+        std::cout << "Input " << -~i << " element of array: ";
+        std::cin >> a.storage[i];
+	
+    }
 	minMaxHThread = CreateThread(NULL, 0, min_max, arr, 0, &minMaxIDThread);
 	averageHThread = CreateThread(NULL, 0, average, arr, 0, &averageIDThread);
 	
@@ -77,15 +71,15 @@ int main() {
 	CloseHandle(averageHThread);
 
 
-	for (int i = 0; i < _size; i++) {
+	for (int i = 0; i < size; i++) {
 		if (arr[i] == _min || arr[i] == _max) {
 			arr[i] = avg;
 		}
 	}
-	for (int i = 0; i < _size; i++) {
-		cout << arr[i] << " ";
+	for (int i = 0; i < size; i++) {
+		std::cout << arr[i] << " ";
 	}
-	cout << "\n";
+	std::cout << "\n";
 
 
 	return 0;
